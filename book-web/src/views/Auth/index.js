064596f6ebd1c87d9aabe921f59c9c8634a2b301
JakeLove2018 <18686224781@163.com>
2022-04-20 23:08:49 +0800

    前端界面登录创建

---
+++ book-web/src/views/Auth/index.js
@@ -0,0 +1,12 @@
+import {defineComponent } from 'vue';
+import {UserAddOutlined,UsbOutlined,MailOutlined} from '@ant-design/icons-vue';
+export default defineComponent({
+    components:{
+        UserAddOutlined,
+        UsbOutlined,
+        MailOutlined
+    },
+    setup(){
+        
+    }
+})
---
+++ book-web/src/views/Auth/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="auth">
+      <div class="bg"></div>
+      <div class="title-info">
+          <img src="" alt="">
+          <h2 class="title">图书管理系统后台</h2>
+      </div>
+      <div class="form">
+          <a-tabs>
+              <a-tab-pane key="1" tab="登录">
+                  <div class="item">
+                      <a-input placeholder="账户" size="large">
+                          <template #prefix>
+                                <UserAddOutlined />
+                            </template>
+                      </a-input>
+                  </div>
+                  <div class="item">
+                      <a-input placeholder="密码" size="large">
+                          <template #prefix>
+                                <usb-outlined />
+                            </template>
+                      </a-input>
+                  </div>
+                  <div class="item">
+                      <a href="">忘记密码</a>
+                  </div>
+                  <div class="item">
+                      <a-button type="primary" size="large">登录</a-button>
+                  </div>
+              </a-tab-pane>
+              <a-tab-pane key="2" tab="注册">
+                  <div class="item">
+                      <a-input placeholder="账户" size="large">
+                            <template #prefix>
+                                <UserAddOutlined />
+                            </template>
+                      </a-input>
+                  </div>
+                  <div class="item">
+                      <a-input placeholder="密码" size="large">
+                            <template #prefix>
+                                <usb-outlined />
+                            </template> 
+
+                      </a-input>
+                  </div>
+                  <div class="item">
+                      <a-input placeholder="邀请码" size="large">
+                          <template #prefix>
+                              <mail-outlined />
+                          </template>
+                          
+                      </a-input>
+                  </div>
+                  <div class="item">
+                      <a-button type="primary" size="large">注册</a-button>
+                  </div>
+              </a-tab-pane>
+          </a-tabs>
+      </div>
+  </div>
+</template>
+
+<script src="./index.js"></script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
